49a753c1f9c089b53166d27cdb2b917daa267827
Patrik Ragnarsson <patrik@starkast.net>
2024-10-27 21:55:21 +0100

    Make log out link work

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :boxes
   get "/login", to: "logins#new"
   post "/login", to: "logins#create"
-  get "/logout", to: "logins#destroy"
+  delete "/logout", to: "logins#destroy"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
