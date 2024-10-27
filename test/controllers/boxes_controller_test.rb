1a571ac477ebbaeb90323a094d9f8cd495f5e2ee
Patrik Ragnarsson <patrik@starkast.net>
2024-10-27 02:13:16 +0200

    Fix the controller test

--- test/controllers/boxes_controller_test.rb
+++ test/controllers/boxes_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class BoxesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    post login_path, params: { username: "test", password: "foobar" }
     get boxes_url
     assert_response :success
   end
