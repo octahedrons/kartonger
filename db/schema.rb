6a72088f4feebe2d068232fd590b0e4b0f78521d
Patrik Ragnarsson <patrik@starkast.net>
2024-12-14 12:28:56 +0100

    Changed `null: false` -> `null: true` in reference migration
    ...and then I ran "rake db:migrate:redo"
    
        $ bin/rails db:migrate:redo
        == 20241214112156 AddBoxRefToActions: reverting ===============================
        -- remove_reference(:actions, :box, {:null=>true, :foreign_key=>true})
           -> 0.0226s
        == 20241214112156 AddBoxRefToActions: reverted (0.0251s) ======================
    
        == 20241214112156 AddBoxRefToActions: migrating ===============================
        -- add_reference(:actions, :box, {:null=>true, :foreign_key=>true})
           -> 0.0102s
        == 20241214112156 AddBoxRefToActions: migrated (0.0102s) ======================

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_16_182611) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_14_112156) do
+  create_table "actions", force: :cascade do |t|
+    t.string "name"
+    t.string "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "box_id"
+    t.index ["box_id"], name: "index_actions_on_box_id"
+  end
+
   create_table "boxes", force: :cascade do |t|
     t.integer "number"
     t.string "room"
@@ -20,4 +29,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_16_182611) do
     t.datetime "updated_at", null: false
     t.string "packed_by"
   end
+
+  add_foreign_key "actions", "boxes"
 end
