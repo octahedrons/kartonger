c820b9d88fab658a564e26798c1e86c7b063c8f5
Patrik Ragnarsson <patrik@starkast.net>
2024-12-14 12:28:56 +0100

    Changed `null: false` -> `null: true` in reference migration
    ...and then I ran "rake db:migrate:redo"
    
        $ bin/rails db:migrate:redo
        == 20241214112156 AddBoxRefToActions: reverting ===============================
        -- remove_reference(:actions, :box, {:null=>true, :foreign_key=>true})
           -> 0.0226s
        == 20241214112156 AddBoxRefToActions: reverted (0.0251s) ======================
    
        == 20241214112156 AddBoxRefToActions: migrating ===============================
        -- add_reference(:actions, :box, {:null=>true, :foreign_key=>true})
           -> 0.0102s
        == 20241214112156 AddBoxRefToActions: migrated (0.0102s) ======================

--- db/migrate/20241214112156_add_box_ref_to_actions.rb
+++ db/migrate/20241214112156_add_box_ref_to_actions.rb
@@ -1,5 +1,5 @@
 class AddBoxRefToActions < ActiveRecord::Migration[8.0]
   def change
-    add_reference :actions, :box, null: false, foreign_key: true
+    add_reference :actions, :box, null: true, foreign_key: true
   end
 end
