47dbf6dc9a16da7bda80724e23d59909b2b8e27f
Patrik Ragnarsson <patrik@starkast.net>
2024-12-19 23:09:43 +0100

    Count boxes per room in one SQL query

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,12 +1,7 @@
 class BoxesController < ApplicationController
   def index
     @boxes = Box.order(number: :desc).all
-    @count_by_room = Box
-      .rooms
-      .each_with_object({}) { |room, hsh| hsh[room] = Box.count_by(:room, room) }
-      .sort_by { _2 }
-      .reverse
-      .to_h
+    @count_by_room = Box.group(:room).count.sort_by { _2 }.reverse.to_h
 
     respond_to do |format|
       format.html
